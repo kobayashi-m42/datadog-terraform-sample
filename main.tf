9bc900df48a669e375157d6de80c19f661662709
kobayashi-m42 <m42kobayashi@gmail.com>
2019-06-14 23:54:12 +0900

    :sparkles: #5 Synthetic API Testのリソースを作成

--- main.tf
+++ main.tf
@@ -3,5 +3,12 @@ module "dashboard" {
 }
 
 module "monitor" {
-  source = "./monitor"
+  source             = "./monitor"
+  slack_channel_name = "${var.slack_channel_name}"
+}
+
+module "synthetic" {
+  source             = "./synthetic"
+  slack_channel_name = "${var.slack_channel_name}"
+  api_test_url       = "${var.api_test_url}"
 }
--- synthetic/variable.tf
+++ synthetic/variable.tf
@@ -1,3 +1,8 @@
+variable "api_test_url" {
+  type    = "string"
+  default = ""
+}
+
 variable "slack_channel_name" {
   type    = "string"
   default = ""
